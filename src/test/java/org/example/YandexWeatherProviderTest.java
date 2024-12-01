785ee672b186371b6c320f146a52f96974210f90
vitaly <vitaly.e.belkin@gmail.com>
2024-12-01 03:04:51 +0300

    добавлены константы

--- src/test/java/org/example/YandexWeatherProviderTest.java
+++ src/test/java/org/example/YandexWeatherProviderTest.java
@@ -8,6 +8,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import static org.example.WeatherApp.LATITUDE;
+import static org.example.WeatherApp.LONGITUDE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -17,6 +19,11 @@ import static org.mockito.Mockito.when;
 class YandexWeatherProviderTest {
 
     private static final String API_KEY = "9eaa2a95-8d92-4246-ad17-b00167d27061";
+    public static final int HTTP_OK = 200;
+    public static final int LIMIT = 3;
+    public static final double AVERAGE_TEMP = 15.0;
+    public static final double ERROR_DELTA = 0.01;
+    public static final double CURRENT_TEMP = 15.0;
     private YandexWeatherProvider provider;
     private HttpClient mockHttpClient;
 
@@ -41,14 +48,14 @@ class YandexWeatherProviderTest {
 
         HttpResponse<String> mockHttpResponse = mock(HttpResponse.class);
         when(mockHttpResponse.body()).thenReturn(mockResponse);
-        when(mockHttpResponse.statusCode()).thenReturn(200);
+        when(mockHttpResponse.statusCode()).thenReturn(HTTP_OK);
         when(mockHttpClient.send(any(HttpRequest.class), any(HttpResponse.BodyHandler.class)))
                 .thenReturn(mockHttpResponse);
 
         // Тестируем метод
-        WeatherData weatherData = provider.getCurrentWeather(55.75, 37.62);
+        WeatherData weatherData = provider.getCurrentWeather(LATITUDE, LONGITUDE);
         assertNotNull(weatherData);
-        assertEquals(15.0, weatherData.getCurrentTemperature(), 0.01);
+        assertEquals(CURRENT_TEMP, weatherData.getCurrentTemperature(), ERROR_DELTA);
     }
 
     @Test
@@ -66,12 +73,12 @@ class YandexWeatherProviderTest {
 
         HttpResponse<String> mockHttpResponse = mock(HttpResponse.class);
         when(mockHttpResponse.body()).thenReturn(mockResponse);
-        when(mockHttpResponse.statusCode()).thenReturn(200);
+        when(mockHttpResponse.statusCode()).thenReturn(HTTP_OK);
         when(mockHttpClient.send(any(HttpRequest.class), any(HttpResponse.BodyHandler.class)))
                 .thenReturn(mockHttpResponse);
 
         // Тестируем метод
-        double averageTemp = provider.getAverageTemperature(55.75, 37.62, 3);
-        assertEquals(15.0, averageTemp, 0.01);
+        double averageTemp = provider.getAverageTemperature(LATITUDE, LONGITUDE, LIMIT);
+        assertEquals(AVERAGE_TEMP, averageTemp, ERROR_DELTA);
     }
 }
